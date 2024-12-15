ee0771a88cda8cd8c055fa79f820420f68e7297f
NCMaintenance <149673546+NCMaintenance@users.noreply.github.com>
2024-12-15 20:20:32 +0000

    Update app.py

--- app.py
+++ app.py
@@ -10,10 +10,11 @@ from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
 
-load_dotenv()
-os.getenv("GOOGLE_API_KEY")
-genai.configure(api_key=st.secrets["G_API_KEY"])
+#load_dotenv()
+#os.getenv("GOOGLE_API_KEY")
+#genai.configure(api_key=st.secrets["G_API_KEY"])
 
+genai.configure(api_key=st.secrets["G_API_KEY"])
 # read all pdf files and return text
 
 
